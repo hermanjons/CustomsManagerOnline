8fc03fa31a482cf2bae42d728e0d61762a3fde37
hamzaisik <hamzaisik023334@gmail.com>
2025-03-17 16:23:45 +0300

    new database table field added to paymentmethod model(edicode)

--- customs_general/models.py
+++ customs_general/models.py
@@ -44,9 +44,10 @@ class Port(models.Model):
 class PaymentMethod(models.Model):
     code = models.CharField(max_length=10, unique=True)
     name = models.CharField(max_length=100)
+    edi_code = models.CharField(max_length=100)
 
     def __str__(self):
-        return f"{self.code} - {self.name}"
+        return f"{self.code} - {self.name} / {self.edi_code}"
 
     class Meta:
         verbose_name = "Ödeme Şekilleri"
